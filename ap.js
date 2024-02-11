6fe6efbd022be4657d3521e83f138d7f318ac9c2
Muzammil Ali <153710574+muzammilali11@users.noreply.github.com>
2024-02-12 01:03:29 +0500

    Add files via upload

---
+++ ap.js
@@ -0,0 +1,101 @@
+//Assignment 1
+
+//Q1
+alert("welcome to our Website");
+//Q2
+alert("Error! Please Enter a valid password");
+//Q3
+alert("Hint: Use line break");
+//Q4
+//alert("First message");
+//alert("Second message");
+//alert("Third message"); 
+//Q5
+//alert("Error! Please Enter a valid password");
+//Q7
+//var username;
+//Q8
+//var my name = "muzammil ali";
+//Q9
+//var message = "hello world";
+//alert(message);
+//Q10
+//var name="Muzammil Ali"
+//var Age =18
+//var PhoneNum='03232265792';
+//var Address="Lyari karachi "
+//alert("Name: " + name);
+//alert("Age: " + Age);
+//alert("Phone Number: " + PhoneNum);
+//alert("Address: " + Address);
+//Q11
+//var email="muzammilali2003@gmail.com"
+//alert(email);
+//Q12 and 13
+//var border ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+//alert(border);
+//Q14
+//var age="18 year"
+//alert(age)
+//Q15
+//var N=1;
+//N++;
+//var visit="You visited this website "+N+ "times";
+//alert(visit)
+//Q16
+//var birthday Year=2004;
+//alert("my birthday Year was : " + birthday);
+//Q17
+// var visitorName = "John Doe,";
+ //var quantity = 5;
+ //var productTitle = "T-Shirts (s)";
+ //var storeName = "Outfitter";
+
+ //alert(visitorName + " " + "Ordered" + " " + quantity + " " + productTitle + " " + "from" + " " + storeName + "!");
+//18// // without values
+
+// var variable1;
+// var variable2;
+// var variable3;
+
+// // // with values
+
+// var variable1 = "string";
+// var variable2 = 1234;
+// var variable3 = true;
+
+
+// Q19
+
+// // legal varialble names:
+
+// userName
+// user_name
+// user123
+// user1name
+// userName$
+
+// // illegal variable names:
+
+// user name
+// user-name
+// 123name
+// #name
+// @myName
+
+//Q20
+//Q21
+//var num1=+prompt("Please enter first number: ")
+//var num2=+prompt("Please enter second number: ")
+//var add =  (num1+num2);
+//alert("The sum of addition is: " + add );
+//Q22
+//var subtract = (num1 - num2);
+//alert("The value after sub is: "  + subtract);
+//var multiply = (num1 * num2);
+//alert("The value after mult is :" + multiply);
+//var divide = (num1 / num2);
+//alert ("The value after div is :"+divide);
+//var modulus = (num1 % num2);
+//alert("The remainder after mod is :" + modulus);
+//Q23
